affd3ec21f0f709ab36adfa52087b9db0cd46e7d
Swetlana Mirzoeva <levka008@mail.ru>
2026-03-17 17:02:01 +0300

    Disable package mode for Django project

--- config/settings.py
+++ config/settings.py
@@ -122,17 +122,17 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Для продакшена (когда DEBUG=False)
 if not DEBUG:
-    STATIC_ROOT = '/static/'
-    MEDIA_ROOT = '/media/'
+    STATIC_ROOT = "/static/"
+    MEDIA_ROOT = "/media/"
 
 # REST Framework settings
 REST_FRAMEWORK = {
